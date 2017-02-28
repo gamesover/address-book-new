7413c68a42c219285e3c6d6f70b74bfc593bee5e
Ken Ding <ken.ding@mail.com>
2017-02-28 22:13:25 +1030

    add tslint loader

--- client/config/webpack.common.js
+++ client/config/webpack.common.js
@@ -42,6 +42,14 @@ module.exports = {
         test: /\.scss$/,
         exclude: /node_modules/,
         loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
+      },
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        options: {
+          configFile: 'tslint.json'
+        }
       }
     ]
   },
