b99458da6364163450793134692f166bb4cf0007
Ken Ding <ken.ding@mail.com>
2017-03-01 22:40:56 +1030

    add address book component

--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -4,7 +4,7 @@ var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'inline-source-map',
 
   output: {
     path: helpers.root('dist'),
@@ -22,8 +22,8 @@ module.exports = webpackMerge(commonConfig, {
     stats: 'minimal',
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
-        secure: false
+        target: 'http://localhost:3000',
+        pathRewrite: {'^/api': ''}
       }
     }
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :address_books, only: [:index, :show, :update, :destroy, :create]
+  resources :address_books, only: :index do
+    collection do
+      post 'upload'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
