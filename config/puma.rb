1f051fa015e28ee90f94e158594447e31cff8a90
Ken Ding <ken.ding@mail.com>
2017-04-04 02:09:09 +0930

    change puma listen to socket

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        3000
+listen      '/tmp/nginx.socket'
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
